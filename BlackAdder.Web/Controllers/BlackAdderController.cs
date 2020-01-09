8d10a90dfb398ec84914806a3f3e260acc8bf73e
Jan Vandepitte <Jan.Vandepitte@sirus.be>
2020-01-09 17:51:44 +0100

    add blackadder to api

---
+++ BlackAdder.Web/Controllers/BlackAdderController.cs
@@ -0,0 +1,20 @@
+﻿using BlackAdder.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackAdder.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlackAdderController : ControllerBase
+    {
+        public IActionResult Add(long one, long two)
+        {
+            return Ok(new Edmund().Add(one, two));
+        }
+
+        public IActionResult Multiply(long one, long two)
+        {
+            return Ok(new Edmund().Multiply(one, two));
+        }
+    }
+}
